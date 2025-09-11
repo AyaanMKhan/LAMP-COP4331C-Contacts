34a2c6a0ba9ca8cbff921ec7a8958b248ef39200
Fsch <Shamu2021@outlook.com>
2025-09-10 21:50:03 -0400

    Tiny ID name mistake fix

--- js/Contacts.js
+++ js/Contacts.js
@@ -24,7 +24,7 @@ function logout()
 
 function CreateContactPop()
 {
-document.getElementById("ContactPopup").style.visibility = "visible";
+document.getElementById("ContactPopupMake").style.visibility = "visible";
 }
 
 function SubmitContact()
